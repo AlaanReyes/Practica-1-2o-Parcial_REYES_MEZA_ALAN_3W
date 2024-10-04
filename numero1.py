8990919e40dfa7fed50a264d2d9469ba6a0a18ba
AlaanReyes <a.23308051281207@cbtis128.edu.mx>
2024-10-03 19:01:08 -0600

    Update numero1.py

--- numero1.py
+++ numero1.py
@@ -1,16 +1,1 @@
-print("Welcome to Project 1!")  # BIENVENIDA AL PROGRAMADOR 
-print(" ")  # ESPACIO
 
-print("REYES MEZA ALAN EDUARDO : PRACTICAS TUPLAS   3W")  # INFO DE PRACTICA Y PROGRAMADOR 
-print(" ")  # ESPACIO
-
-# lista de los precios
-precios = [43, 57, 92, 20, 37, 5, 9]
-
-# Encontrar el menor y el mayor precio
-menor_precio = min(precios)
-mayor_precio = max(precios)
-
-# Mostrar los resultados
-print(f"El menor precio es: {menor_precio}")
-print(f"El mayor precio es: {mayor_precio}")
